1c0bc7e3fa797b165fcc8380085dc6c0d3e8b7cc
Don <100505855+DonFungible@users.noreply.github.com>
2025-04-10 01:04:00 -0700

    remove redundant variable

--- packages/storykit/src/lib/graph.ts
+++ packages/storykit/src/lib/graph.ts
@@ -1,3 +1,4 @@
+import { CHAINID_TO_CHAINNAME } from "@/constants"
 import { shortenAddress } from "@/lib/utils"
 import { Asset, NFTMetadata } from "@/types"
 import { RESOURCE_TYPE } from "@/types/api"
@@ -7,11 +8,6 @@ import { Address } from "viem"
 
 import { listResource } from "./api"
 import { NFT, getNFTByTokenId, getNFTByTokenIds } from "./simplehash"
-
-export const CHAINID_TO_CHAINNAME: { [key: number]: string } = {
-  1315: "story-aeneid",
-  1514: "story",
-}
 
 export interface GraphNode {
   id: string
