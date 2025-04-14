4004561db7c7a2d7984be34d0fbfa32c75cf964e
samfairbairn <samfairbairn@hotmail.co.uk>
2025-04-14 21:04:44 +0800

    add useIpAssets story

--- packages/storykit/src/hooks/useIpAssets/index.ts
+++ packages/storykit/src/hooks/useIpAssets/index.ts
@@ -1,7 +1,7 @@
 import { paths } from "@storykit/api-schema"
 import { Address } from "viem"
 
-import { type UseListQueryOptions, useListQuery } from "./useListQuery"
+import { type UseListQueryOptions, useListQuery } from "../useListQuery"
 
 // This extracts the options type from the OpenAPI schema for the /api/v3/assets POST endpoint
 type Options = paths["/api/v3/assets"]["post"]["requestBody"]["content"]["application/json"]["options"]
