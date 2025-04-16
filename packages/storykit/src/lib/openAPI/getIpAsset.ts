f644cf677b67a0e03ff360a0ad191a6390b09f6f
samfairbairn <samfairbairn@hotmail.co.uk>
2025-04-16 10:15:51 +0800

    getQuery, getIpAsset lib function

---
+++ packages/storykit/src/lib/openAPI/getIpAsset.ts
@@ -0,0 +1,13 @@
+import { paths } from "@storykit/api-schema"
+
+import { getQuery } from "./getQuery"
+
+export function getIpAsset({ ipId, chainName, apiKey }: { ipId: string; chainName: string; apiKey: string }) {
+  return getQuery({
+    path: "/api/v3/assets/{assetId}",
+    pathParams: { assetId: ipId },
+    additionalHeaders: { "X-Extend-Asset": "true" },
+    chainName,
+    apiKey,
+  })
+}
