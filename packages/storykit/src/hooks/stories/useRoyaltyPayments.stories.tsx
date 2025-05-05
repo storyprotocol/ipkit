0626ff9f7bcde92a91fa31789808ceada3d66636
samfairbairn <samfairbairn@hotmail.co.uk>
2025-05-05 23:17:43 +0800

    useRoyaltyPayments POST req api file, hook and story

---
+++ packages/storykit/src/hooks/stories/useRoyaltyPayments.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import React from "react"
+import { formatEther } from "viem"
+
+import { UseRoyaltyPaymentsOptions, useRoyaltyPayments } from "../useRoyaltyPayments"
+import { DataTable } from "./(components)/DataTable"
+
+const Example = (args: UseRoyaltyPaymentsOptions) => {
+  const { isLoading, data } = useRoyaltyPayments(args)
+  const fields = ["id", "amount", "sender", "payerIpId", "receiverIpId"]
+
+  if (isLoading) return <>loading...</>
+  if (!data?.data) return <>none found</>
+
+  return (
+    <DataTable
+      fields={fields}
+      data={data.data.map((payment) => ({
+        ...payment,
+        amount: `${Number(formatEther(BigInt(payment.amount || 0))).toFixed(3)}IP`,
+      }))}
+    />
+  )
+}
+
+const meta = {
+  title: "Hooks/useRoyaltyPayments",
+  component: Example,
+  parameters: {
+    layout: "centered",
+  },
+  args: {
+    options: {
+      pagination: {
+        limit: 10,
+      },
+    },
+    queryOptions: {
+      enabled: true,
+    },
+  },
+} satisfies Meta<typeof Example>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {},
+}
