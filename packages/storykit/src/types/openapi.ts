66903f0975279cb393aa4958de63c6faff5d7104
samfairbairn <samfairbairn@hotmail.co.uk>
2025-05-20 19:44:21 +0800

    add remaining OpenAPI types

--- packages/storykit/src/types/openapi.ts
+++ packages/storykit/src/types/openapi.ts
@@ -22,3 +22,33 @@ export type LicenseToken =
 
 export type RoyaltyPay =
   components["schemas"]["github_com_storyprotocol_protocol-api_api_internal_models_protocolv1.RoyaltyPay"]
+
+export type IPGroup =
+  components["schemas"]["github_com_storyprotocol_protocol-api_api_internal_models_protocolv2.IPGroup"]
+
+export type IPGroupEdge =
+  components["schemas"]["github_com_storyprotocol_protocol-api_api_internal_models_protocolv2.IPGroupEdge"]
+
+export type DetailedTerms =
+  components["schemas"]["github_com_storyprotocol_protocol-api_api_internal_models_protocolv2.DetailedTerms"]
+
+export type Transaction =
+  components["schemas"]["github_com_storyprotocol_protocol-api_api_internal_models_protocolv1.Transaction"]
+
+export type Permission =
+  components["schemas"]["github_com_storyprotocol_protocol-api_api_internal_models_protocolv1.Permission"]
+
+export type Module =
+  components["schemas"]["github_com_storyprotocol_protocol-api_api_internal_models_protocolv1.Module"]
+
+export type LicenseTemplate =
+  components["schemas"]["github_com_storyprotocol_protocol-api_api_internal_models_protocolv1.LicenseTemplate"]
+
+export type LicenseMintingFeePaid =
+  components["schemas"]["github_com_storyprotocol_protocol-api_api_internal_models_protocolv1.LicenseMintingFeePaid"]
+
+export type Dispute =
+  components["schemas"]["github_com_storyprotocol_protocol-api_api_internal_models_protocolv1.Dispute"]
+
+export type Collection =
+  components["schemas"]["github_com_storyprotocol_protocol-api_api_internal_models_protocolv1.Collection"]
