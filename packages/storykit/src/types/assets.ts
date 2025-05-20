92d2e3e4bc6fbb019f18791434d10aaf6051fe4c
samfairbairn <samfairbairn@hotmail.co.uk>
2025-05-20 20:23:48 +0800

    restore some types used in monorepo

--- packages/storykit/src/types/assets.ts
+++ packages/storykit/src/types/assets.ts
@@ -1,3 +1,5 @@
+import { Address } from "viem"
+
 export enum PIL_FLAVOR {
   NON_COMMERCIAL_SOCIAL_REMIXING = "Non-Commercial Social Remixing",
   COMMERCIAL_USE = "Commercial Use",
@@ -22,10 +24,44 @@ export interface Trait {
 
 // TODO: add offchain data
 
-// export interface LicenseOffChainData {
-//   aiLearningModels?: boolean
-// }
+export interface LicenseOffChainData {
+  aiLearningModels?: boolean
+}
 
 // export interface PILTermsWithOffChainData extends PILTerms {
 //   offChainData: LicenseOffChainData | undefined
 // }
+
+export type SocialMedia = {
+  platform?: string
+  url?: string
+}
+
+export type Creator = {
+  name?: string
+  address?: Address
+  description?: string
+  contributionPercent?: number
+  socialMedia?: SocialMedia[]
+}
+
+export interface IPMetadata {
+  title?: string
+  description?: string
+  ipType?: string
+  creators?: Creator[]
+  appInfo?: {
+    id?: string
+    name?: string
+    website?: string
+  }[]
+  relationships?: {
+    parentIpId?: Address
+    type?: string
+  }[]
+  robotTerms?: {
+    userAgent?: string
+    allow?: string
+  }
+  [key: string]: any
+}
