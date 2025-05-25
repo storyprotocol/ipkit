e0f1353adc2aa7ea66f5524e766f755150072025
samfairbairn <samfairbairn@hotmail.co.uk>
2025-05-25 12:14:29 +0800

    update examples

--- examples/custom-theme/app/IpHeader.tsx
+++ examples/custom-theme/app/IpHeader.tsx
@@ -5,11 +5,12 @@ export default function IpHeader() {
   return (
     <div className="flex w-full flex-col gap-1">
       <h1 className="text-2xl font-sans font-bold text-foreground">{nftData?.name}</h1>
-      <h2 className="text-xs text-gray-500">By {nftData?.owners[0].owner_address}</h2>
-      {nftData?.previews.image_medium_url ? 
+      {/* TODO: get owners */}
+      {/* <h2 className="text-xs text-gray-500">By {nftData?.owners[0].owner_address}</h2> */}
+      {nftData?.image.thumbnailUrl || nftData?.image.cachedUrl ? 
         // eslint-disable-next-line @next/next/no-img-element
         <img 
-          src={nftData.previews.image_medium_url} 
+          src={nftData?.image.thumbnailUrl || nftData?.image.cachedUrl} 
           alt={nftData?.name || "No image available"} 
           className="w-full aspect-video object-contain border border-gray-200 rounded-md my-2" 
         /> 
