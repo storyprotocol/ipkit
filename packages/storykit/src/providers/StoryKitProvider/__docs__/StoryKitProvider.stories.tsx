ccf77977482b500dd9479bb0418cedbdebc48988
samfairbairn <samfairbairn@hotmail.co.uk>
2025-08-28 19:41:22 +0800

    simplify StoryKitProvider with isTestnet prop

--- packages/storykit/src/providers/StoryKitProvider/__docs__/StoryKitProvider.stories.tsx
+++ packages/storykit/src/providers/StoryKitProvider/__docs__/StoryKitProvider.stories.tsx
@@ -1,4 +1,3 @@
-import { STORYKIT_SUPPORTED_CHAIN, WRAPPED_IP } from "@/types/chains"
 import type { Meta, StoryObj } from "@storybook/react"
 
 import Example from "./Example"
@@ -11,13 +10,12 @@ const meta = {
     disableStoryProvider: true,
   },
   argTypes: {
-    chain: { control: "select", options: Object.values(STORYKIT_SUPPORTED_CHAIN) },
-    defaultCurrency: { control: "select", options: [WRAPPED_IP] },
+    isTestnet: { control: "boolean" },
     theme: { control: "select", options: ["default", "story"] },
     mode: { control: "select", options: ["auto", "light", "dark"] },
   },
   args: {
-    chain: STORYKIT_SUPPORTED_CHAIN.STORY_MAINNET,
+    isTestnet: true,
     theme: "default",
     mode: "auto",
   },
@@ -31,8 +29,7 @@ export const Aeneid: Story = {
     children: { control: false },
   },
   args: {
-    chain: STORYKIT_SUPPORTED_CHAIN.AENEID_TESTNET,
-    defaultCurrency: WRAPPED_IP,
+    isTestnet: true,
   },
 }
 
@@ -41,8 +38,7 @@ export const Mainnet: Story = {
     children: { control: false },
   },
   args: {
-    chain: STORYKIT_SUPPORTED_CHAIN.STORY_MAINNET,
-    defaultCurrency: WRAPPED_IP,
+    isTestnet: false,
   },
 }
 
