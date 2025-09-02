4d0469f19e669f9ea1ddd788d60145e8fab17c16
samfairbairn <samfairbairn@hotmail.co.uk>
2025-09-02 11:29:37 +0800

    update examples

--- examples/custom-theme/app/IpHeader.tsx
+++ examples/custom-theme/app/IpHeader.tsx
@@ -5,12 +5,13 @@ export default function IpHeader() {
   const { data, isLoading } = useIpAsset({ ipId: "0xB1D831271A68Db5c18c8F0B69327446f7C8D0A42" })
 
   if (isLoading) return <div>Loading...</div>
+  if (!data) return <div>Not found</div>
 
   return (
     <div className="flex w-full flex-col gap-1">
       <h1 className="text-2xl font-sans font-bold text-foreground">{data.title}</h1>
       <h2 className="text-xs text-gray-500">By {data.ownerAddress}</h2>
-      {data.nftMetadata.image.cachedUrl || data.nftMetadata.image.pngUrl ? 
+      {data.nftMetadata?.image.cachedUrl || data.nftMetadata?.image.pngUrl ? 
         // eslint-disable-next-line @next/next/no-img-element
         <img 
           src={data.nftMetadata.image.cachedUrl || data.nftMetadata.image.pngUrl} 
