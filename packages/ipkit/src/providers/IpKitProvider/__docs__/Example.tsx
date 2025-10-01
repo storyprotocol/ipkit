222cbc518fe153335c14f5125dd8891febe2288a
samfairbairn <samfairbairn@hotmail.co.uk>
2025-10-01 18:10:06 +0800

    disable theme and mode

--- packages/ipkit/src/providers/IpKitProvider/__docs__/Example.tsx
+++ packages/ipkit/src/providers/IpKitProvider/__docs__/Example.tsx
@@ -1,34 +1,27 @@
-import { cn } from "@/lib"
 import React, { FC } from "react"
 
 import "../../../global.css"
-import { IpKitProvider, Theme, useIpKit } from "../IpKitProvider"
+import { IpKitProvider, useIpKit } from "../IpKitProvider"
 
 const TESTNET_API_KEY = process.env.STORYBOOK_STORY_PROTOCOL_X_TESTNET_API_KEY as string
 const API_KEY = process.env.STORYBOOK_STORY_PROTOCOL_X_API_KEY as string
 
 const Example: FC<{
   isTestnet: boolean
-  theme: Theme
-  mode: "auto" | "light" | "dark"
+
   children?: React.ReactNode
-}> = ({ isTestnet, theme, mode, children = <ExampleComponent /> }) => {
+}> = ({ isTestnet, children = <ExampleComponent /> }) => {
   return (
-    <IpKitProvider
-      apiKey={isTestnet ? TESTNET_API_KEY : API_KEY}
-      isTestnet={isTestnet}
-      theme={theme}
-      mode={mode !== "auto" ? mode : undefined}
-    >
+    <IpKitProvider apiKey={isTestnet ? TESTNET_API_KEY : API_KEY} isTestnet={isTestnet}>
       {children}
     </IpKitProvider>
   )
 }
 
 const ExampleComponent = () => {
-  const { themeClass, chain, apiBaseUrl } = useIpKit()
+  const { chain, apiBaseUrl } = useIpKit()
   return (
-    <div className={cn(themeClass, "flex flex-col h-full bg-background p-8 font-sans text-primary")}>
+    <div className={"flex flex-col h-full bg-background p-8 font-sans text-primary"}>
       <div className="text-sm text-foreground">
         <i>id:</i> <strong>{chain.id}</strong>
       </div>
